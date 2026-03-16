1b029feb54b982e37916f15f6caf38696ce022f6
Iaroslav Krasnokutskii <Yroslavkr1995@gmail.com>
2026-03-16 14:52:24 +0100

    Initial Commit

---
+++ LinkedinArticles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkedinArticles
+//
+//  Created by Iaroslav Krasnokutskii on 16. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkedinArticles/LinkedinArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinArticlesApp.swift
+//  LinkedinArticles
+//
+//  Created by Iaroslav Krasnokutskii on 16. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct LinkedinArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
